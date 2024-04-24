e64b8974306a6b4592b1d1d30ea24277f317900b
Travis James <travis@tribehealthsolutions.com>
2024-04-24 09:53:58 -0500

    improvements

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,9 @@ import (
 	"github.com/supabase-community/supabase-go"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 	"tribemedia.io/m/ent"
 	"tribemedia.io/m/handlers"
@@ -22,10 +25,21 @@ import (
 	s "tribemedia.io/m/supabase"
 )
 
+// Open initializes the Ent client with a connection to the database.
 func Open(databaseUrl string) *ent.Client {
-	db, err := sql.Open("pgx", databaseUrl)
+	var db *sql.DB
+	var err error
+	// Implementing a retry mechanism for database connection
+	for i := 0; i < 5; i++ {
+		db, err = sql.Open("pgx", databaseUrl)
+		if err == nil {
+			break
+		}
+		log.Printf("Failed to connect to database. Retry in 5 seconds...")
+		time.Sleep(5 * time.Second)
+	}
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to connect to database after retries: %v", err)
 	}
 
 	// Create an ent.Driver from `db`.
@@ -34,22 +48,23 @@ func Open(databaseUrl string) *ent.Client {
 }
 
 func main() {
-
+	// Define command-line flags
 	pflag.String("dsn", "", "database connection string")
+	pflag.Int("port", 8180, "Port to run the server on")
 	pflag.Parse()
 
 	// Bind the command line flags to Viper
-	err := viper.BindPFlags(pflag.CommandLine)
-	if err != nil {
+	if err := viper.BindPFlags(pflag.CommandLine); err != nil {
 		log.Fatal(err)
 	}
 
+	// Configuration file setup
 	configFile := viper.GetString("config")
 	if configFile != "" {
 		viper.SetConfigFile(configFile)
 	} else {
-		viper.SetConfigName("config") // Name of the config file (without extension)
-		viper.AddConfigPath(".")      // Look for config in the current directory
+		viper.SetConfigName("config")
+		viper.AddConfigPath(".")
 	}
 
 	// Attempt to read the configuration file
@@ -60,92 +75,74 @@ func main() {
 	// Automatically read configuration options from environment variables
 	viper.AutomaticEnv()
 
-	//dsn := "postgresql://postgres:postgres@127.0.0.1:4322/postgres"
-
 	// Use the configuration values
 	dsn := viper.GetString("dsn")
 	supabaseUrl := viper.GetString("supabaseUrl")
 	supabaseAnonKey := viper.GetString("supabaseAnonKey")
 	supabaseServiceRoleKey := viper.GetString("supabaseServiceRoleKey")
 	projectRef := viper.GetString("projectRef")
+	port := viper.GetInt("port") // Using the port number from Viper
 
-	fmt.Println("DSN:", dsn)
-	fmt.Println("Supabase URL:", supabaseUrl)
-	fmt.Println("Supabase Anon Key:", supabaseAnonKey)
-	fmt.Println("Supabase Service Role Key:", supabaseServiceRoleKey)
-	fmt.Println("Project Ref:", projectRef)
-
+	// Database client initialization with retry logic
 	client := Open(dsn)
-	defer func(client *ent.Client) {
-		err := client.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(client)
+	defer client.Close()
 
-	// Run the auto migration tool.
+	// Auto migration
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
 
-	supabaseClient, err := supabase.NewClient(
-		supabaseUrl, supabaseServiceRoleKey, nil)
+	// Supabase client setup
+	supabaseClient, err := supabase.NewClient(supabaseUrl, supabaseServiceRoleKey, nil)
 	if err != nil {
-		fmt.Println("cannot initalize client", err)
+		log.Fatalf("cannot initialize Supabase client: %v", err)
 	}
 
-	goTrueClient := gotrue.New(
-		projectRef,
-		supabaseAnonKey,
-	)
-
+	// GoTrue client setup for Supabase authentication
+	goTrueClient := gotrue.New(projectRef, supabaseAnonKey)
 	authService := services.NewAuthService(goTrueClient)
 
-	//secretRepo := entgo.NewSecretRepositoryEntgo(client)
-	//functionRepo := entgo.NewFunctionRepositoryEntgo(client)
+	// Repository and handler initialization
 	customHostnameRepo := entgo.NewCustomHostnameRepositoryEntgo(client)
-	//networkBanRepo := entgo.NewNetworkBanRepositoryEntgo(client)
-	//pgsodiumConfigRepo := entgo.NewPgsodiumConfigRepositoryEntgo(client)
 	projectRepo := entgo.NewProjectRepositoryEntgo(client, supabaseClient)
-	apiHandler := handlers.NewApiHandler(
-		projectRepo,
-		customHostnameRepo,
-	)
+	apiHandler := handlers.NewApiHandler(projectRepo, customHostnameRepo)
 
-	// Create generated server.
+	// Generated server setup
 	srv, err := s.NewServer(apiHandler, handlers.NewSecurityHandler(authService))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Create a new router
+	// HTTP server and router setup
 	router := http.NewServeMux()
-
-	// Register the server routes
 	router.Handle("/", srv)
-
-	// Register the Swagger UI endpoint
 	opts := middleware.SwaggerUIOpts{SpecURL: "/openapi.json"}
 	sh := middleware.SwaggerUI(opts, nil)
 	router.Handle("/docs", sh)
-
-	// Register the openapi.json endpoint
 	router.HandleFunc("/openapi.json", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./openapi.json")
 	})
 
-	// Start the server
-	if err := http.ListenAndServe(":8180", router); err != nil {
-		log.Fatal(err)
-	}
-}
+	// Graceful shutdown setup
+	httpSrv := &http.Server{Addr: fmt.Sprintf(":%d", port), Handler: router}
+	go func() {
+		log.Printf("Server is running at http://localhost:%d", port)
+		if err := httpSrv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
 
-func globalLoggingHook(next ent.Mutator) ent.Mutator {
-	return ent.MutateFunc(func(ctx context.Context, m ent.Mutation) (ent.Value, error) {
-		start := time.Now()
-		defer func() {
-			log.Printf("Op=%s\tType=%s\tTime=%s\tConcreteType=%T\n", m.Op(), m.Type(), time.Since(start), m)
-		}()
-		return next.Mutate(ctx, m)
-	})
+	// Signal handling for graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := httpSrv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
